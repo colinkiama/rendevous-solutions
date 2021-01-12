f9d9a0f760e00bf0c87d654c484f0c7a09e0c280
Colin Kiama <colinkiama@gmail.com>
2021-01-12 08:01:32 +0000

    Accept input from command line

--- Q1/index.js
+++ Q1/index.js
@@ -1,1 +1,11 @@
-console.log("Hello!");
+import readlineSync from "readline-sync";
+const I = 1;
+const V = 5;
+const L = 50;
+const C = 100;
+const D = 500;
+const M = 1000;
+
+let numberToConvert = readlineSync.question("Enter a number to convert to Roman Numerals: ")
+console.log("Number:", numberToConvert);
+
